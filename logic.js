5fb2ebf54a666a173030f47111d042b459f50844
Reily Maahs <reily.maahs@gmail.com>
2023-01-16 21:51:28 -0500

    Add basic movement and wall collision

--- logic.js
+++ logic.js
@@ -1,6 +1,10 @@
+let gameBoard = "";
+let snake = "";
+
 window.addEventListener('load', () => {
-    let gameBoard = [
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    snake = createSnakeHead();
+    gameBoard = [
+        ['head', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '', '', 'apple', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
@@ -14,7 +18,7 @@ window.addEventListener('load', () => {
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', 'body', 'body', 'head', ''],        
+        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],        
     ]
     const game = document.querySelector('.game');
     const board = document.querySelector('.board');
@@ -31,6 +35,10 @@ window.addEventListener('load', () => {
         shadeElement.style.borderRadius = '1%'
         return shadeElement;
     })();
+
+
+    // Add the movement
+    document.addEventListener('keydown', (e) => moveSnake(e, gameBoard));
 
     game.append(shade);
 
@@ -66,7 +74,13 @@ window.addEventListener('load', () => {
         },1);   
     }
 
-    drawBoard(gameBoard, board);
+    setInterval(() => {
+        //moveSnake(gameBoard)
+        clearBoard(board);
+        drawBoard(gameBoard, board);
+    }, (1 / 60) * 1000);
+
+    
 }, false);
     
 /*
@@ -85,7 +99,7 @@ function drawBoard(currentBoardState, board) {
             // Draw each of the elements to the screen
             switch (currentBoardState[i][j]) {
                 case "head":
-                    let snakeHead = createSnakeHead();
+                    let snakeHead = snake.head.element;
                     snakeHead.style.top = (32 * i) + 'px'
                     snakeHead.style.left = (32 * j) + 'px'
                     board.append(snakeHead);
@@ -108,14 +122,111 @@ function drawBoard(currentBoardState, board) {
     }
 }
 
+function clearBoard(board) {
+    while (board.lastElementChild) {
+        board.removeChild(board.lastElementChild);
+    }
+}
+
+function moveSnake(event, currentBoardState) {
+    let snakeHead = snake.head; 
+    // https://stackoverflow.com/a/5597114
+    switch(event.key) {
+        case 'ArrowLeft': 
+            // left
+            if (checkInBounds(snake.head.x, snake.head.y - 1)) {
+                currentBoardState = updateBoard(snake.head.x, snake.head.y, "", currentBoardState);
+                currentBoardState = updateBoard(snake.head.x, snake.head.y - 1, "head", currentBoardState);
+                updateSnakeHeadLocation(currentBoardState);
+            } else {
+                console.log("Not in bounds")
+            }
+        break;
+        case 'ArrowRight': 
+            // right
+            if (checkInBounds(snake.head.x, snake.head.y + 1)) {
+                currentBoardState = updateBoard(snake.head.x, snake.head.y, "", currentBoardState);
+                currentBoardState = updateBoard(snake.head.x, snake.head.y + 1, "head", currentBoardState);
+                updateSnakeHeadLocation(currentBoardState);
+            } else {
+                console.log("Not in bounds")
+            }
+        break;
+        case 'ArrowUp': 
+            // up
+            if (checkInBounds(snake.head.x - 1, snake.head.y)) {
+                currentBoardState = updateBoard(snake.head.x, snake.head.y, "", currentBoardState);
+                currentBoardState = updateBoard(snake.head.x - 1, snake.head.y, "head", currentBoardState);
+                updateSnakeHeadLocation(currentBoardState);
+            } else {
+                console.log("Not in bounds")
+            }
+        break;
+        case 'ArrowDown': 
+            // down
+            if (checkInBounds(snake.head.x + 1, snake.head.y)) {
+                currentBoardState = updateBoard(snake.head.x, snake.head.y, "", currentBoardState);
+                currentBoardState = updateBoard(snake.head.x + 1, snake.head.y, "head", currentBoardState);
+                updateSnakeHeadLocation(currentBoardState);
+            } else {
+                console.log("Not in bounds")
+            }
+        break;
+    }
+}
+
+function updateSnakeHeadLocation(currentBoardState) {
+    for (let i = 0; i < 15; i++) {
+        for (let j = 0; j < 15; j++) {
+            // Draw each of the elements to the screen
+            if (currentBoardState[i][j] == 'head') {
+                snake.head.x = i;
+                snake.head.y = j;
+                snake.head.element.innerText = "x: " + snake.head.x + ", y: " + snake.head.y;
+            } 
+        }
+    }
+    return -1;
+}
+
+function checkInBounds(x, y) {
+    if (x < 0 || x > 14) {
+        return false;
+    } 
+
+    if (y < 0 || y > 14) {
+        return false;
+    } 
+
+    return true;
+}
+
+function updateBoard(x, y, type, currentBoardState) {
+    currentBoardState[x][y] = type;
+    this.board = currentBoardState;
+    return this.board;
+}
+
 function createSnakeHead() {
+    let snake = {
+        head: {
+            element: "",
+            x: 0,
+            y: 0
+        },
+        body: []
+    };
+
     let snakeHead = document.createElement('div');
     snakeHead.style.width = '32px';
     snakeHead.style.height = '32px';
     snakeHead.style.backgroundColor = '#a9fc03';
     snakeHead.style.position = 'absolute';
     snakeHead.style.borderRadius = '1%'
-    return snakeHead;
+
+    snake.head.element = snakeHead;
+
+    return snake;
 }
 
 function createSnakeBody() {
